0d669cf9cd332ff2772891518f84925c07202229
NicholasPianfetti <145074360+NicholasPianfetti@users.noreply.github.com>
2025-03-04 21:15:31 -0600

    opens with sidebar closed, only HERMA is in Times, sidebar controls always on left, tooltips always visible, min width changed

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -34,7 +34,7 @@ const Home: React.FC = () => {
   const [uploadedFiles, setUploadedFiles] = useState<string[]>([]);
   const [clickTimers, setClickTimers] = useState<ClickTimer>({});
   const [selectedFiles, setSelectedFiles] = useState<string[]>([]);
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const[showAlert, setShowAlert] = useState(false);
   const [isStreaming, setIsStreaming] = useState(false);
 
@@ -484,8 +484,8 @@ const handleSubmit = async (event: React.FormEvent) => {
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
                         fill="currentColor"
-                        width="25"
-                        height="25"
+                        width="20"
+                        height="20"
                       >
                         <path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10zM8 13.01l1.41 1.41L11 12.84V17h2v-4.16l1.59 1.59L16 13.01 12.01 9 8 13.01z"/>
                       </svg>
@@ -622,8 +622,8 @@ const handleSubmit = async (event: React.FormEvent) => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          width="25"
-          height="25"
+          width="20"
+          height="20"
         >
           <path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10zM8 13.01l1.41 1.41L11 12.84V17h2v-4.16l1.59 1.59L16 13.01 12.01 9 8 13.01z"/>
         </svg>
@@ -702,8 +702,8 @@ const handleSubmit = async (event: React.FormEvent) => {
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
                     fill="currentColor"
-                    width="25"
-                    height="25"
+                    width="20"
+                    height="20"
                   >
                     <path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10zM8 13.01l1.41 1.41L11 12.84V17h2v-4.16l1.59 1.59L16 13.01 12.01 9 8 13.01z"/>
                   </svg>
